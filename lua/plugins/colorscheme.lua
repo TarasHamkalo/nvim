71e50963d0cf0898505f8ebb4f9cfd070edd2f78
Taras Hamkalo <taras.hamkalo@student.tuke.sk>
2023-10-30 17:05:18 +0100

    #

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,33 @@
 return {
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		priority = 1000,
-		config = function(_)
-			vim.cmd.colorscheme("rose-pine-moon")
-		end,
-	},
-	{
-		"ellisonleao/gruvbox.nvim",
-		name = "gruvbox",
-		priority = 1000,
-		-- config = function(_)
-		-- 	vim.o.background = "black"
-		-- 	vim.cmd.colorscheme("gruvbox")
-		-- end,
-	},
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        priority = 1000,
+        -- config = function(_)
+        --     vim.cmd.colorscheme("rose-pine-moon")
+        -- end,
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        name = "gruvbox",
+        priority = 1000,
+        -- config = function(_)
+        -- 	vim.o.background = "black"
+        -- 	vim.cmd.colorscheme("gruvbox")
+        -- end,
+    },
+    {
+        "blazkowolf/gruber-darker.nvim",
+        name = "darker",
+        opts = {
+            bold = false,
+            italic = {
+                strings = false,
+            },
+        },
+        priority = 1000,
+        config = function(_)
+            vim.cmd.colorscheme("gruber-darker")
+        end,
+    }
 }
