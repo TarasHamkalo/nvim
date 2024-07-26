d9bb5de67689e98920fa45fb9276f0f56af57ad6
Taras Hamkalo <taras.hamkalo@student.tuke.sk>
2024-07-26 18:32:35 +0200

    nvim dap ui dependencies changed

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -1,5 +1,9 @@
 return {
 	"rcarriga/nvim-dap-ui",
+  dependencies = {
+    "mfussenegger/nvim-dap",
+    "nvim-neotest/nvim-nio"
+  },
 	event = "VeryLazy",
 	config = function(_)
 		require("configs.nvim-dap-ui")
